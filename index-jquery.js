c80d0885633b63c5fa214817b18c1436fe486c51
sjones88 <steven.g.jones88@gmail.com>
2015-12-20 17:55:28 -0600

    Update css, html and jquery for index page

--- index-jquery.js
+++ index-jquery.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
-	$('.about-me1').hide();
-	$('.about-me2').hide();
-	$('.about-me3').hide();
-	$('section h3').hide();
-	$('section h3').slideDown('slow');
+	$('section').hide();
 	$('header').hide();
-	$('header').slideDown('slow');
-	$('.about-me1').fadeIn(500);
-	$('.about-me2').fadeIn(1000);
-	$('.about-me3').fadeIn(1500);
+	$('header').slideDown(2500);
+	$('section').fadeIn(2500);
 });
